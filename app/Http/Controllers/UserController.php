07c69a576a1bdaf04de7a2431ef1f94e59df67a3
Hassan Bagheri <h5.baghery@gmail.com>
2025-03-02 05:27:22 +0330

    validations

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'username' => ['required', 'min:3', 'max:20', Rule::unique(table: 'users', column: 'username')],
+            'email' => ['required', 'email', Rule::unique(table: 'users', column: 'email')],
+            'password' => ['required', 'min:8', 'confirmed']
         ]);
         User::create($incomingFields);
         return "hello from register";
