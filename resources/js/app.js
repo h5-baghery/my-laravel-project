6a7f08e4ba42a46dec2a17d9eaf4b93aafaaf808
Hassan Bagheri <h5.bagheri@gmail.com>
2025-05-06 22:53:44 +0330

    Coming back, Working on Search ended, run build done, tailwind romves from app.css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,9 @@
 import './bootstrap';
+import Search from './live-search'
 import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.start();
+
+if (document.querySelector(".header-search-icon")) {
+    new Search();
+}
